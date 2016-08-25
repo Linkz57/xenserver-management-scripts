4b3ff41c0d599435dcdef7e5324733c23b70a999
Tyler Francis <Linkz57@gmail.com>
2016-08-25 10:12:50 -0500

    Added log cleanup to someone else's code

--- xenbackup_edits.sh
+++ xenbackup_edits.sh
@@ -21,3 +21,11 @@ fi
 #
 # Version 1.2 of Mr. Rahul Kumar's scipt doesn't seem any different than the version 1.1 that I'm using,
 # so you should be able to plug my lines anywhere in between his lines 23 and 35.
+
+
+
+# At the very end of Mr. Rahul Kumar's script I have added the line
+
+\rm backup_failed*.log
+
+# which should clean up all failure reports after a single successful backup.
