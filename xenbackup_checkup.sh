0a08a3f9cf281110a4bf2cbcbe11245e005b60ba
Tyler Francis <Linkz57@gmail.com>
2016-10-21 11:14:24 -0500

    Stop liking this script? It'll help you kill it.

--- xenbackup_checkup.sh
+++ xenbackup_checkup.sh
@@ -4,11 +4,28 @@
 # on 2016-08-24
 # Website: github.com/linkz57
 #
-# Version 0.1
+# Version 1.0
 #
 # The idea is to make sure xenbackup is working.
 # No one likes silent failures for their backups
 # so this will watch for errors created by xenbackup.sh
+
+SCRIPTPATH=`pwd -P`/xenbackup_checkup.sh
+hostname=`hostname`
+
+
+# Thanks to Christopher Neylan for the following statement using grep's exit code for if conditional
+# https://stackoverflow.com/questions/9422461/check-if-directory-mounted-with-bash
+
+if ssh root@OMITTED "ls -laFh /root/backup*" | grep failed > xenbackup_fail.mail; then
+	echo "Failure report found"
+	cat xenbackup_fail.mail | mail -s "XenBackup ran and failed now or in the past. If I'm wrong or wasting your attention, feel free to edit me at $SCRIPTPATH on $hostname" me@jelec.com
+else
+	echo "XenBackup has not reported failure. This is either good or really bad"
+fi
+
+
+
 
 
 ## on Debian, I set up the mailer with
@@ -18,14 +35,3 @@
 ## to get ssh working automatically (not requiring a password)
 ## use ssh-keygen and ssh-copy-id
 ## I followed a guide here: http://www.thegeekstuff.com/2008/11/3-steps-to-perform-ssh-login-without-password-using-ssh-keygen-ssh-copy-id
-
-
-# Thanks to Christopher Neylan for the following statement using grep's exit code for if conditional
-# https://stackoverflow.com/questions/9422461/check-if-directory-mounted-with-bash
-
-if ssh root@OMITTED "ls -laFh /root/backup*" | grep failed > xenbackup_fail.mail; then
-	echo "Failure report found"
-	cat xenbackup_fail.mail | mail -s "XenBackup ran and failed now or in the past" tyler.francis@jelec.com
-else
-	echo "XenBackup has not reported failure. This is either good or really bad"
-fi
