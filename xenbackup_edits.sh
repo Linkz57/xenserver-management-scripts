454d56acead9c967efd57df4f339cc4d93fd2286
Tyler Francis <Linkz57@gmail.com>
2017-12-13 15:40:05 -0600

    Capture relivant logs during backups

--- xenbackup_edits.sh
+++ xenbackup_edits.sh
@@ -7,11 +7,29 @@
 # I mount my backup share at /vmbackup
 
 
+
+
+
+## Add this bit to the top
+## Capture logs during these backups
+function mahLogz() {
+        tail -f /var/log/SMlog | grep -v discs > $BACKUPPATH/smlog_during_backup_of_$1.log
+}
+
+
+
+
+
+
+
+
+
+
 if mount | grep /vmbackup > /dev/null; then
         echo "mount exists, will continue"
 else
         echo "Mount failed. Make sure the FreeNAS box is on and sharing via CIFS. I'm gonna quit now without backing anything up." > backup_failed_$DATE.log
-        exit 0
+        exit 1
 fi
 
 
@@ -24,8 +42,26 @@ fi
 
 
 
-# At the very end of Mr. Rahul Kumar's script I have added the line
+
+
+
+
+## Add this to the while loop, right after the VMNAME variable is set
+        mahLogz $VMNAME &
+        mahLogzPID=$!
+
+
+
+## add this to the very end of the while loop, after 'xe vm-uninstall...'
+
+        sleep 600
+        kill $mahLogzPID
+
+
+
+# At the very end of Mr. Rahul Kumar's script I have added these two lines
 
 \rm backup_failed*.log
+date +%s > success.log
 
 # which should clean up all failure reports after a single successful backup.
